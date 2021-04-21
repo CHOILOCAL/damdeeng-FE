0c3f8e84ffc753c7389677b8e87248fed2ebd800
Choilocal <hyunchoi@hist.co.kr>
2021-04-21 13:39:49 +0900

    react-async로 변경

--- src/Users.js
+++ src/Users.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import useAsync from './useAsync';
+import {useAsync} from 'react-async';
 import User from './User';
 
 async function getUsers() {
@@ -9,28 +9,24 @@ async function getUsers() {
 }
 function Users() {
 
-    // const [users, setUsers] = useState(null);
-    // const [loading, setLoading] = useState(false);
-    // const [error, setError] = useState(null);
-
-    const [state, refetch] = useAsync(getUsers, [], true);
     const [userId, setUserId] = useState(null);
-
-    const { loading, data: users, error } = state;
+    const { data: users, error, loading, reload, run } = useAsync({
+        deferFn: getUsers
+    });
 
     if(loading) {
         return <div>로딩중</div>
     } else if (error) {
         return <div>에러발생</div>
     } else if (!users) {
-        return <button onClick={refetch}>불러오기</button>;
+        return <button onClick={reload}>불러오기</button>;
     } 
 
     return <><ul>
         {users.map(user => <li key={user.id} onClick={() => setUserId(user.id)}>
             {user.username} ({user.name})
         </li>)}
-        <button onClick={refetch}>다시 불러오기</button>
+        <button onClick={reload}>다시 불러오기</button>
         {userId && <User id={userId} />}
     
     </ul></>;
