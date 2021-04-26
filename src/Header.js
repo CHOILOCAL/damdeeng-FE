2984f4134b9d5283e6448cc74d7124fcdc2950a1
choilocal <hyunchoi@hist.co.kr>
2021-04-26 15:15:36 +0900

    add a bootstrap

--- src/Header.js
+++ src/Header.js
@@ -1,29 +1,39 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import {Nav, FormControl, Form} from 'react-bootstrap';
 
 
 function Header() {
     return (
-      <div>
-        <ul>
-        <li>
-          <Link to="/">담딩투어 소개</Link>
-        </li>
-        <li>
-          <Link to="/products">투어 코스</Link>
-        </li>
-        <li>
-          <Link to="/custom">여행 만들기</Link>
-        </li>
-        <li>
-          <Link to="/makeTogether">동행찾기</Link>
-        </li>
-        <li>
-          <Link to="/qna">QnA</Link>
-        </li>
-      </ul>
-        
-      </div>
+
+      <>
+  <Nav className="justify-content-center" activeKey="/home">
+    <Nav.Item>
+      <Nav.Link eventKey="link-1" href="/products">투어 코스</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="link-2" href="/custom">여행 만들기</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="link-2" href="/makeTogether">동행찾기</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="link-2" href="/qna">QnA</Nav.Link>
+    </Nav.Item>
+    <Nav.Item>
+      <Nav.Link eventKey="disabled" disabled>
+        Disabled
+      </Nav.Link>
+    </Nav.Item>
+
+    <Form inline>
+      {/* <FormControl type="text" placeholder="검색어를 입력해주세요. " className="mr-sm-2" /> */}
+      <button type="button" class="btn btn-outline-secondary">로그인</button>
+      <button type="button" class="btn btn-outline-secondary">회원가입</button>
+    </Form>
+
+  </Nav>
+</>
     );
   }
   
